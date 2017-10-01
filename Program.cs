df428ee9ec0e660647731444cf55d6487051d6cb
RudyMeijer <RudyMeijer@gmail.com>
2017-10-01 20:54:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//
+			// To insert a quote in a literal C# string add an extra quote.
+			//
+			var s = @"25;""text1"";18.0;303.50;""text2a;text2b;text2c"";2017-05-14 23:38:05;""woof"";;10";
+			var result = StateParse(s);
+			foreach (var str in result) Console.WriteLine(str);
+		}
+		/// <summary>
+		/// Because there are ; inside a string you need a parser with state machine.
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns>array of strings</returns>
+		private static string[] StateParse(string s)
+		{
+			var i = 0;
+			var state=false;
+			var sb = new StringBuilder(s);
+			foreach (var c in s)
+			{
+				if (c == '"') state = !state; // The state machine.
+				if (c == ';' && !state) sb[i] = ',';
+				i++;
+			}
+			return sb.ToString().Split(',');
+		}
+	}
+}
